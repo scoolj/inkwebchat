d6b88cfd2e4a6ed8fc18339ea6d6ce987d2a85f7
Scoolj <scoolj99@gmail.com>
2020-07-02 06:32:43 +0100

    admin notification  and emoji

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Input.css";
+
+const Input = ({ message, setMessage, sendMessage }) => (
+  <form className="form">
+    <input
+      className="input"
+      type="text"
+      type="text"
+      placeholder="Type a message..."
+      value={message}
+      onChange={ ({target:{value}}) => setMessage(value)}
+      onKeyPress={(event) =>
+        event.key === "Enter" ? sendMessage(event) : null
+      }
+    />
+    <button className="sendButton" onClick={(event) => sendMessage(event)}>
+      Send
+    </button>
+  </form>
+);
+
+export default Input;
