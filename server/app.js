e9a73d77a02d5950458555fbb9076cfcd8b69c1b
Scoolj <scoolj99@gmail.com>
2020-07-26 09:52:27 +0100

    login

--- server/app.js
+++ server/app.js
@@ -4,8 +4,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
+const { auth } = require("./middlewares/auth");
 const db = require("./models");
-const { user } = require("./models");
+// const { User } = require("./models");
 const Role = db.role;
 
 db.mongoose
@@ -27,54 +28,69 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // authentication
-
-
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    username: req.user.username,
+    email: req.user.email,
+    userstate: req.user.userstate,
+    usercountry: req.user.usercountry,
+    role: req.user.roles,
+  });
+});
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
-      userData: doc
+      userData: doc,
     });
   });
 });
 
-
-app.post('/api/user/login', (req, res) =>{
-    // find the username
-    User.findOne({username: res.body.username}, (err, user) =>{
-        if(!user)
-        return res.json({
-            loginSuccess: false,
-            message: "Auth Failed, username not found"
-        })
-    })
+app.post("/api/user/login", (req, res) => {
+  // find the username
+  User.findOne({ username: req.body.username }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth Failed, username not found",
+      });
 
     // compare password
-    user.comparePassword(req.body.password, (err, isMatch) =>{
-        if(!isMatch){
-            return res.json({ 
-                loginSuccess: false,
-                 message: "Wrong password" })
-        }
-    })
-
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Wrong password",
+        });
+      }
+    });
 
     //generateToken
-    user.generateToken((err, user) =>{
-        if(err) return res.status(400).send(err)
-        res.cookie("x_auth", user.token)
-        .status(200)
-        .json({
-            loginSuccess:true
-        })
-    })
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
+  });
+});
 
+// logout function
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 
-})
 // set port, listen for requests
 const PORT = process.env.PORT || 5030;
 app.listen(PORT, () => {
--- server/models/User.js
+++ server/models/User.js
@@ -1,74 +1,90 @@
 const mongoose = require("mongoose");
- const bcrypt = require('bcrypt')
- const saltRounds =10
- const jwt = require('jsonwebtoken')
- const userShema = mongoose.Schema({
-    username: {
-      type: String,
-      maxlength: 40,
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    maxlength: 50,
+    unique: 1,
+  },
+  userstate: {
+    type:String,
+  },
+  usercountry:{
+    type: String,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role",
     },
-    userstate: String,
-    usercountry: String,
-    email: {
-      type: String,
-      trim: true,
-      unique: 1,
-    },
-    password: {
-      type: String,
-      minlength: 5,
-    },
-    token: {
-      type: String,
-    },
-    tokenExp: {
-      type: Number,
-    },
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role",
-      },
-    ],
-  })
+  ],
+});
 
-  userShema.pre('save', function(next){
-    var user = this
-    if(user.isModified('password')){
-    bcrypt.genSalt(saltRounds, function(err, salt){
-      if(err) return next(err)
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
 
-      bcrypt.hash(user.password, salt, function(err, hash){
-        if(err) return next(err)
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
 
-        user.password = hash
-        next()
-      })
-    })
-  } else{
-    next ()
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
   });
+};
 
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secret");
 
-  userShema.methods.comparePassword = function (plainPassword, cb){
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-      if(err) return cb(err)
-      cb(null, isMatch)
-    })
-  }
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
-  userShema.methods.generateToken = function(cb){
-    var user = this;
-    var token = jwt.sign(user._id.toHexString(), 'secret')
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
 
-    user.token = token;
-    user.save(function(err, user){
-      if(err) return cb(err)
-      cb(null, user)
-    })
-  }
+  jwt.verify(token, "secret", function (err, decode) {
+    user.findOne({ "_id": decode, "token": token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
 
-const User = mongoose.model('User', userShema)
+  });
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
